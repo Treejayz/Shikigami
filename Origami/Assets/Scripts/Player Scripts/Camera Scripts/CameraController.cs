0eb22ecb9fe4ef3b751e29a2a180b63bde3fd2d0
CJ Desktop <legatc@rpi.edu>
2018-02-06 21:01:54 -0500

    Created a better camera script, and tested having the player face the direction of the camera.

---
+++ Origami/Assets/Scripts/Player Scripts/Camera Scripts/CameraController.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+
+    private const float Y_ANGLE_MIN = 0.0f;
+    private const float Y_ANGLE_MAX = 70.0f;
+
+    public Transform target;
+
+    private Camera cam;
+    private float maxDistance = 10.0f;
+    private float currentX = 0.0f;
+    private float currentY = 0.0f;
+    private float sensX = 4.0f;
+    private float sensY = 1.0f;
+
+    // Use this for initialization
+    private void Start () {
+        cam = Camera.main;
+	}
+	
+	// Update is called once per frame
+	private void Update () {
+        currentX += Input.GetAxis("Mouse X") * sensX;
+        currentY += Input.GetAxis("Mouse Y") * sensY;
+
+        currentY = Mathf.Clamp(currentY, Y_ANGLE_MIN, Y_ANGLE_MAX);
+        if (currentX < 0.0f) { currentX += 360; }
+        if (currentX > 360.0f) { currentX -= 360; }
+	}
+
+    private void LateUpdate()
+    {
+        Vector3 dir = new Vector3(0, 0, -maxDistance);
+        Quaternion rotation = Quaternion.Euler(currentY, currentX, 0);
+        this.transform.position = target.position + rotation * dir;
+        this.transform.LookAt(target.position);
+    }
+}
---
+++ Origami/Assets/Scripts/Player Scripts/forwardtest.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class forwardtest : MonoBehaviour {
+
+    public Transform camTrans;
+    public Transform camTargetTrans;
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+        Vector3 camDir = camTargetTrans.position- camTrans.position;
+        camDir = new Vector3(camDir.x, 0.0f, camDir.z);
+        camDir = camDir / camDir.magnitude;
+        this.transform.rotation = Quaternion.LookRotation(Vector3.Slerp(this.transform.forward, camDir, 0.5f));
+	}
+}
