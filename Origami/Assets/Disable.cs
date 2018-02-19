b714723ffb775ff852c0fe5be8d2427c64db164e
Kalvin Janik <janikk@rpi.edu>
2018-02-19 17:19:57 -0500

    Menu basics working

---
+++ Origami/Assets/Disable.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Disable : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		this.GetComponent<Canvas> ().enabled = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Origami/Assets/Menu_controls.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Menu_controls : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void loadScene(string scenename){
+		SceneManager.LoadScene (scenename);
+	}
+	public void loadCanvas(GameObject canvasName){
+		this.GetComponent<Canvas> ().enabled = false;
+		canvasName.GetComponent<Canvas> ().enabled = true;
+	}
+	public void quit(){
+		Application.Quit();
+	}
+}
