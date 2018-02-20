b0a14439afe7703dec0c3db96855fd7b8de4813e
CJ Legato <legatc@rpi.edu>
2018-02-20 10:53:05 -0500

    Collectables! Right now it's just a pill, but keeps track of pieces collected and plays a little effect for each piece.

---
+++ Origami/Assets/Scripts/Level Scripts/Collectable.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Collectable : MonoBehaviour {
+
+	public enum CollectableType
+    {
+        PIECE,
+        PAGE
+    };
+
+    public CollectableType Type = CollectableType.PIECE;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            CollectableManager.Collect();
+            GetComponent<ParticleSystem>().Play();
+            GetComponent<Collider>().enabled = false;
+            GetComponent<MeshRenderer>().enabled = false;
+            StartCoroutine("Kill");
+        }
+    }
+
+    IEnumerator Kill()
+    {
+        yield return new WaitForSeconds(1f);
+        Destroy(this.gameObject);
+    }
+}
