ed42ed1497e1b133be5a36e6990505404611b7f2
Kalvin Janik <janikk@rpi.edu>
2018-02-19 22:20:47 -0500

    Transform UI basic on and off working

---
+++ Origami/Assets/Scripts/UI Scripts/Tform_Icon_Mangement.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tform_Icon_Mangement : MonoBehaviour {
+	
+	public GameObject pause;
+	public GameObject player;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+	// Update is called once per frame
+	void Update () {
+		if (pause.GetComponent<Canvas> ().enabled) {
+			this.GetComponent<Canvas> ().enabled = false;
+		} else {
+			this.GetComponent<Canvas> ().enabled = true;
+		}
+		if (!player.GetComponent<Character>().canFrog) {
+			this.GetComponent<Canvas> ().enabled = false;
+		}
+	}
+}
