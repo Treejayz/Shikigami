8a5af399bce542ca32cee83ca91ed9541aa400d2
CJ Legato <legatc@rpi.edu>
2018-02-21 00:35:05 -0500

    Paper pieces now have a sprite that spins and its fun

--- Origami/Assets/Scripts/Level Scripts/Collectable.cs
+++ Origami/Assets/Scripts/Level Scripts/Collectable.cs
@@ -12,6 +12,13 @@ public class Collectable : MonoBehaviour {
 
     public CollectableType Type = CollectableType.PIECE;
 
+    public float spinSpeed;
+
+    private void Update()
+    {
+        transform.Rotate(0f, spinSpeed * Time.deltaTime, 0f);
+    }
+
     private void OnTriggerEnter(Collider other)
     {
         if (other.tag == "Player")
@@ -19,7 +26,9 @@ public class Collectable : MonoBehaviour {
             CollectableManager.Collect();
             GetComponent<ParticleSystem>().Play();
             GetComponent<Collider>().enabled = false;
-            GetComponent<MeshRenderer>().enabled = false;
+            GetComponent<SpriteRenderer>().enabled = false;
+            Component halo = GetComponent("Halo");
+            halo.GetType().GetProperty("enabled").SetValue(halo, false, null);
             StartCoroutine("Kill");
         }
     }
