cb2d4fbd754a1781123040d7817a642f1cece6b2
CJ Legato <legatc@rpi.edu>
2018-02-21 01:16:32 -0500

    Frog page yay

--- Origami/Assets/Scripts/Trigger Scripts/FrogPage.cs
+++ Origami/Assets/Scripts/Trigger Scripts/FrogPage.cs
@@ -4,15 +4,26 @@ using UnityEngine;
 
 public class FrogPage : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    public float spinSpeed;
+    public float frequency;
+    public float amplitude;
+
+    private float startY;
+
+    private void Start()
+    {
+        startY = transform.position.y;
+    }
+
+    // Update is called once per frame
+    void Update () {
+        transform.Rotate(0f, spinSpeed * Time.deltaTime, 0f);
+
+        float tempY = startY;
+        tempY += Mathf.Sin(Time.fixedTime * Mathf.PI * frequency) * amplitude;
+
+        transform.position = new Vector3(transform.position.x, tempY, transform.position.z);
+    }
 
     public void OnTriggerEnter(Collider other)
     {
