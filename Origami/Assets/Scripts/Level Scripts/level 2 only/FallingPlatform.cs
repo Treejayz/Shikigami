c54e0438b9d969231ec307680101c3153fda2e7e
CJ Legato <legatc@rpi.edu>
2018-03-05 20:13:56 -0500

    Falling platforms that fall and eventually pop back into existance

--- Origami/Assets/Scripts/Level Scripts/level 2 only/FallingPlatform.cs
+++ Origami/Assets/Scripts/Level Scripts/level 2 only/FallingPlatform.cs
@@ -4,15 +4,27 @@ using UnityEngine;
 
 public class FallingPlatform : MonoBehaviour {
 
+    float shakeTime = .7f;
+    float fallTime = 1f;
+    float shakeAmount = 0.2f;
+    float fallAcceleration = 30f;
+
+    float respawntime = .5f;
+    public AnimationCurve popIn;
+
     public Transform plat;
-    private Transform platStart;
+    private Vector3 platStartPos;
+    private Vector3 platStartScale;
     public Transform platVisual;
-    public Transform platVisualStart;
+    private Vector3 platVisualStart;
 
     private bool triggered;
 
     void Start () {
         triggered = false;
+        platStartPos = plat.position;
+        platVisualStart = platVisual.position;
+        platStartScale = plat.localScale;
 	}
 	
 	// Update is called once per frame
@@ -31,6 +43,50 @@ public class FallingPlatform : MonoBehaviour {
 
     IEnumerator Shake()
     {
-        yield return null;
+        float currentTime = 0f;
+        while (currentTime < shakeTime)
+        {
+            float currentRatio = (shakeTime - currentTime) / shakeTime;
+            float x = Random.Range(currentRatio * -1f, currentRatio) * shakeAmount;
+            float z = Random.Range(currentRatio * -1f, currentRatio) * shakeAmount;
+            platVisual.position = new Vector3(platVisualStart.x + x, platVisualStart.y, platVisualStart.z + z);
+            currentTime += Time.fixedDeltaTime;
+            yield return new WaitForFixedUpdate();
+        }
+        StartCoroutine("Fall");
+    }
+
+    IEnumerator Fall()
+    {
+        float currentSpeed = 0f;
+        float currentTime = 0f;
+        while (currentTime < fallTime)
+        {
+            currentSpeed += fallAcceleration * Time.fixedDeltaTime;
+            plat.transform.position += Vector3.down * currentSpeed * Time.fixedDeltaTime;
+            plat.transform.localScale = platStartScale * ((fallTime - currentTime) / fallTime);
+
+            currentTime += Time.fixedDeltaTime;
+            yield return new WaitForFixedUpdate();
+        }
+        plat.gameObject.SetActive(false);
+        yield return new WaitForSeconds(3f);
+        StartCoroutine("Respawn");
+    }
+
+    IEnumerator Respawn()
+    {
+        plat.gameObject.SetActive(true);
+        plat.transform.position = platStartPos;
+        plat.transform.localScale = new Vector3(0f, 0f, 0f);
+        float currentTime = 0f;
+        while (currentTime < respawntime)
+        {
+            float currentScale = popIn.Evaluate(currentTime / respawntime);
+            plat.transform.localScale = platStartScale * currentScale;
+            currentTime += Time.fixedDeltaTime;
+            yield return new WaitForFixedUpdate();
+        }
+        triggered = false;
     }
 }
