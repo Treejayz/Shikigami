7fe37ecaefa334e2fec8db13419909ddd10164c7
CJ Desktop <legatc@rpi.edu>
2018-03-23 15:40:29 -0400

    I have begun making the dragon shoot shadow bullets at you but it does not yet work

---
+++ Origami/Assets/Scripts/Level Scripts/level 3 only/ShadowBullet.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShadowBullet : MonoBehaviour {
+
+    public Vector3 target;
+    public float speed;
+    private float rotationspeed;
+
+    private float rotateTime = 2f;
+
+    private float targetRotX;
+    private float xRate;
+    private float targetRotY;
+    private float yRate;
+    private float targetRotZ;
+    private float zRate;
+
+    // Use this for initialization
+    void Start () {
+        targetRotX = Random.Range(50f, 300f);
+        targetRotY = Random.Range(50f, 300f);
+        targetRotY = Random.Range(50f, 300f);
+        xRate = Random.Range(-300f, 300f);
+        yRate = Random.Range(-300f, 300f);
+        zRate = Random.Range(-300f, 300f);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (Mathf.Abs(yRate - targetRotY) < 10f)
+        {
+            if (targetRotY < 0f)
+            {
+                targetRotY = Random.Range(50f, 200f);
+            } else
+            {
+                targetRotY = Random.Range(-200f, -50f);
+            }
+        }
+        if (Mathf.Abs(xRate - targetRotX) < 10f)
+        {
+            if (targetRotX < 0f)
+            {
+                targetRotX = Random.Range(50f, 200f);
+            }
+            else
+            {
+                targetRotX = Random.Range(-200f, -50f);
+            }
+        }
+        if (Mathf.Abs(zRate - targetRotZ) < 10f)
+        {
+            if (targetRotZ < 0f)
+            {
+                targetRotZ = Random.Range(50f, 200f);
+            }
+            else
+            {
+                targetRotZ = Random.Range(-200f, -50f);
+            }
+        }
+        xRate = Mathf.Lerp(xRate, targetRotX, 1f * Time.deltaTime);
+        yRate = Mathf.Lerp(yRate, targetRotY, 1f * Time.deltaTime);
+        zRate = Mathf.Lerp(zRate, targetRotZ, 1f * Time.deltaTime);
+    }
+
+    private void FixedUpdate()
+    {
+        transform.position = Vector3.MoveTowards(transform.position, target, speed * Time.fixedDeltaTime);
+
+        transform.Rotate(xRate * Time.fixedDeltaTime, yRate * Time.fixedDeltaTime, zRate * Time.fixedDeltaTime);
+    }
+}
---
+++ Origami/Assets/Scripts/Level Scripts/level 3 only/ShadowFire.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShadowFire : MonoBehaviour {
+
+    public GameObject shadowBullet;
+    public GameObject player;
+
+    float fireTime;
+
+	// Use this for initialization
+	void Start () {
+        fireTime = 0f;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        fireTime += Time.deltaTime;
+        if (fireTime > 1f)
+        {
+            StartCoroutine("CollectData");
+            fireTime = 0f;
+        }
+	}
+
+    IEnumerator CollectData()
+    {
+        float time = 0f;
+        List<Vector3> list = new List<Vector3>();
+
+        Vector3 prev = player.transform.position;
+        Vector3 current;
+        yield return new WaitForSeconds(0.05f);
+        time += 0.05f;
+        while (time < 0.5f)
+        {
+            current = player.transform.position;
+            Vector3 temp = (current - prev);
+            list.Add(temp);
+            prev = current;
+
+            yield return new WaitForSeconds(0.05f);
+            time += 0.05f;
+        }
+
+        Vector3 result = new Vector3(0f, 0f, 0f);
+        int i = 0;
+
+        foreach (Vector3 vec in list)
+        {
+            result += vec;
+            i += 1; 
+        }
+        result = result / i;
+        Vector3 target = calculateTarget(result);
+        GameObject bullet = Instantiate(shadowBullet,transform.position, Quaternion.identity);
+        bullet.GetComponent<ShadowBullet>().target = target;
+        bullet.GetComponent<ShadowBullet>().speed = 15f;
+    }
+
+    private Vector3 calculateTarget(Vector3 velocity)
+    {
+
+
+        return (player.transform.position);
+    }
+}
