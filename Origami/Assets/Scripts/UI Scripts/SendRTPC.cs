38980b20310ad7195e769dc3da3f05443d431853
Axel <stewaa5@rpi.edu>
2018-03-27 19:23:31 -0400

    Edited RTPC Script to allow persistence of volume slider values between scenes.

--- Origami/Assets/Scripts/UI Scripts/SendRTPC.cs
+++ Origami/Assets/Scripts/UI Scripts/SendRTPC.cs
@@ -10,7 +10,10 @@ public class SendRTPC : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-		
+		if(PlayerPrefs.HasKey(RTPC))
+        {
+            mainSlider.value = PlayerPrefs.GetFloat(RTPC);
+        }
 	}
 	
 	// Update is called once per frame
@@ -19,7 +22,7 @@ public class SendRTPC : MonoBehaviour {
 	}
 
     public void ValueChanged () {
-
+        PlayerPrefs.SetFloat(RTPC, mainSlider.value);
         AkSoundEngine.SetRTPCValue(RTPC, mainSlider.value, null);
     }
 }
