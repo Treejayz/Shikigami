4ef35838a820e176d90c89666c730c3b5438cb33
Kalvin Janik <janikk@rpi.edu>
2018-03-27 23:17:46 -0400

    Tutorial text script and prefab working

--- Origami/Assets/Scripts/UI Scripts/BookSystem.cs
+++ Origami/Assets/Scripts/UI Scripts/BookSystem.cs
@@ -3,7 +3,8 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class BookSystem : MonoBehaviour {
-
+	int activepage1;
+	int activepage2;
 	// Use this for initialization
 	void Start () {
 		
---
+++ Origami/Assets/Scripts/UI Scripts/TutorialDisplay.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TutorialDisplay : MonoBehaviour {
+	public string displayText;
+	public GameObject displayCanvas;
+	float fadeTime;
+
+	// Use this for initialization
+	void Start () {
+		fadeTime= 1.5f;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	public void OnTriggerEnter(Collider other){
+		if (other.gameObject.tag == "Player") {
+			displayCanvas.GetComponentInChildren<Text> ().text = displayText;
+			StartCoroutine("DisplayText");
+			new WaitForSeconds(7.0f);
+		}
+	}
+
+	private IEnumerator DisplayText()
+	{
+		Color tempcoltext = displayCanvas.GetComponentInChildren<Text>().color;
+		Color tempcolimage = displayCanvas.GetComponentInChildren<Image>().color;
+		while (tempcoltext.a < 1f)
+		{
+			float newAlpha = displayCanvas.GetComponentInChildren<Text>().color.a + (Time.fixedDeltaTime * (1f / fadeTime));
+			if (newAlpha > 1f) { newAlpha = 1f; }
+			tempcoltext = displayCanvas.GetComponentInChildren<Text>().color;
+			tempcoltext.a = newAlpha;
+			tempcolimage = displayCanvas.GetComponentInChildren<Image>().color;
+			tempcolimage.a = newAlpha;
+			displayCanvas.GetComponentInChildren<Image>().color = tempcolimage;
+			displayCanvas.GetComponentInChildren<Text> ().color = tempcoltext;
+			yield return new WaitForFixedUpdate();
+		}
+		yield return new WaitForSeconds(3.0f);
+		while (tempcoltext.a > 0f)
+		{
+			float newAlpha = displayCanvas.GetComponentInChildren<Text>().color.a - (Time.fixedDeltaTime * (1f / fadeTime));
+			if (newAlpha < 0f) { newAlpha = 0f; }
+			tempcoltext = displayCanvas.GetComponentInChildren<Text>().color;
+			tempcoltext.a = newAlpha;
+			tempcolimage = displayCanvas.GetComponentInChildren<Image>().color;
+			tempcolimage.a = newAlpha;
+			displayCanvas.GetComponentInChildren<Image>().color = tempcolimage;
+			displayCanvas.GetComponentInChildren<Text> ().color = tempcoltext;
+			yield return new WaitForFixedUpdate();
+		}
+		Destroy (this.gameObject);
+	}
+}
