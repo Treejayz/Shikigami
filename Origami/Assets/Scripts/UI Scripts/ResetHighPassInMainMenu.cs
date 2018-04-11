bddc414876db7fc83bf19b3f7f4e1283acba38e9
Axel <stewaa5@rpi.edu>
2018-04-11 14:58:35 -0400

    Lowered walk-flap volume. Fixed main menu so that high pass does not persist when returning from pause menu.

---
+++ Origami/Assets/Scripts/UI Scripts/ResetHighPassInMainMenu.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ResetHighPassInMainMenu : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        AkSoundEngine.SetRTPCValue("MenuUp", 0.0f, null);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
