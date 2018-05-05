f37c744b8924806ed9b947f1148f16f3f0f64f0c
Kalvin Janik <janikk@rpi.edu>
2018-05-05 02:06:42 -0400

    Controller corners things or whatever

---
+++ Origami/Assets/Scripts/UI Scripts/ControllerImage.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class ControllerImage : MonoBehaviour {
+	public Sprite control;
+	public Sprite keyboard;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetJoystickNames ().GetLength(0) == 0) {
+			this.gameObject.GetComponent<Image> ().sprite = keyboard;
+		} else {
+			this.gameObject.GetComponent<Image> ().sprite = control;
+		}
+	}
+}
