b8823594ff8f4daeaa6a293af079f85c4d123fb8
Claire Thomas <clairethomas711@gmail.com>
2018-05-05 02:55:23 -0400

    Hey look the trees now blow in the wind and the menu looks slightly more organized around the image

---
+++ Origami/Assets/Scripts/UI Scripts/UIMenuPos.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIMenuPos : MonoBehaviour {
+	public float percentX;
+	public float percentY;
+	float x;
+	float y;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+		float current_width = (Screen.width-1920)/2;
+		float current_height = (Screen.height-1080)/2;
+		x = 1920 * percentX + current_width;
+		y = 1080 * percentY + current_height;
+		transform.position = new Vector3 (x, y, transform.position.z);
+	}
+}
